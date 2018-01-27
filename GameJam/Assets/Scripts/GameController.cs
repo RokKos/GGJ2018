319632bd7c4454f34250e19fced28722f69202f1
Rok Kos <rocki5555@gmail.com>
2018-01-27 11:37:00 +0100

    Camera Prefab and Changing between map and text

---
+++ GameJam/Assets/Scripts/GameController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+    [SerializeField] GameObject MorseText;
+    [SerializeField] GameObject MapImage;
+
+    private bool IsTextActive = true;
+
+    // Use this for initialization
+    void Start () {
+        EnableScreenObjects();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.M)) {
+            IsTextActive = !IsTextActive;
+            EnableScreenObjects();
+        }
+	}
+
+    void EnableScreenObjects () {
+        MorseText.SetActive(IsTextActive);
+        MapImage.SetActive(!IsTextActive);
+    }
+}
