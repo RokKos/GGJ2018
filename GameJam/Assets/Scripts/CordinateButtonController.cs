9cd826592db39ed22a78e6da7221fbd9cb4c8e90
Rok Kos <rocki5555@gmail.com>
2018-01-27 13:10:06 +0100

    Firs draft for map and buttons that return value

---
+++ GameJam/Assets/Scripts/CordinateButtonController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CordinateButtonController : MonoBehaviour {
+
+    [SerializeField] Button Button;
+    [SerializeField] Text TextCordinate;
+
+    private int CordinateX = -1;
+    private int CordinateY = -1;
+
+    // Use this for initialization
+    void Start () {
+        Button.onClick.AddListener(RecordCordinate);
+    }
+
+    void RecordCordinate () {
+        Debug.Log("You have clicked the button!");
+        Debug.Log(CordinateX + " " + CordinateY);
+    }
+
+    public void SetterOfCordinate (int x, int y) {
+        CordinateX = x;
+        CordinateY = y;
+        TextCordinate.text = x.ToString() + "," + y.ToString();
+    }
+}
---
+++ GameJam/Assets/Scripts/MapController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapController : MonoBehaviour {
+
+    [SerializeField] CordinateButtonController PrefabButton;
+
+    const int LeftMostSide = -450;
+    const int UpMostSide = 200;
+    // Use this for initialization
+    void Start () {
+        for (int y = 0; y < 10; ++y) {
+            for (int x = 0; x < 10; ++x) {
+                CordinateButtonController obj = Instantiate(PrefabButton, this.transform);
+                obj.transform.localPosition = new Vector3(LeftMostSide + x * 50, UpMostSide - y * 50);
+                obj.SetterOfCordinate(x, y);
+                obj.name = "Cord: X:" + x.ToString() + " Y:" + y.ToString();
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
