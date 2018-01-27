4783438a8ca28a4079b6a3b92fd9db25816db8d9
DomenPigeon <domen.golob.px@gmail.com>
2018-01-27 18:24:23 +0100

    HearType prefab updated

---
+++ GameJam/Assets/Scripts/UserInputedText.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UserInputedText : MonoBehaviour {
+
+    Text textElement;
+
+	// Use this for initialization
+	void Start () {
+        textElement = GetComponent<Text>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.anyKeyDown) {
+            textElement.text += Input.inputString;
+        }
+	}
+}
