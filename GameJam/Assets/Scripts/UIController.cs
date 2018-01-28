1db82ef593eef7a00fb841c7090cd6a0efdfa4be
Rok Kos <rocki5555@gmail.com>
2018-01-28 10:18:05 +0100

    Info Panel and credits

--- GameJam/Assets/Scripts/UIController.cs
+++ GameJam/Assets/Scripts/UIController.cs
@@ -8,20 +8,28 @@ public class UIController : MonoBehaviour {
 
     [SerializeField] GameController GameController;
     [SerializeField] GameObject MainCanvas;
+    [SerializeField] GameObject InfoPanel;
     [SerializeField] Image EndImage;
     [SerializeField] Sprite EndSprite;
 
     const float CountDownTimer = 3.0f;
     float CurrentTime = 0;
     bool coundownStarted = false;
+    bool InfoOn = false;
 
 
     private void Start () {
         MainCanvas.SetActive(false);
         coundownStarted = false;
+        InfoPanel.SetActive(InfoOn);
         EndImage.enabled = false;
     }
     private void Update () {
+        if (Input.GetKeyDown(KeyCode.E)) {
+            ShowInfo();
+        }
+
+
         if (!coundownStarted) {
             return;
         }
@@ -50,5 +58,11 @@ public class UIController : MonoBehaviour {
     private void QuitGame () {
         Debug.Log("Quit");
         Application.Quit();
+    }
+
+    private void ShowInfo () {
+        InfoOn = !InfoOn;
+        InfoPanel.SetActive(InfoOn);
+        MainCanvas.SetActive(InfoOn);
     }
 }
