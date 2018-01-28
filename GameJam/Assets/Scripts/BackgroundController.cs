e74a86963c4b9c43a08b87bb834b58adfc518d4a
Rok Kos <rocki5555@gmail.com>
2018-01-28 14:02:12 +0100

    Sounds, Instruction changes, Playing background sound

---
+++ GameJam/Assets/Scripts/BackgroundController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundController : MonoBehaviour {
+
+    [SerializeField] AudioClip IntroSound;
+    [SerializeField] AudioSource AudioSource;
+
+    // Use this for initialization
+    void Start () {
+        AudioSource.clip = IntroSound;
+        AudioSource.PlayOneShot(IntroSound);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(AudioSource.isPlaying);
+	}
+}
