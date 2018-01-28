4b0854619ffd728869cceb400f465f7bce8e6133
Rok Kos <rocki5555@gmail.com>
2018-01-28 14:50:32 +0100

    Sound logic and looping

--- GameJam/Assets/Scripts/BackgroundController.cs
+++ GameJam/Assets/Scripts/BackgroundController.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class BackgroundController : MonoBehaviour {
 
     [SerializeField] AudioClip IntroSound;
+    [SerializeField] AudioClip LoopSoundStart;
+    [SerializeField] AudioClip LoopSoundLoop;
     [SerializeField] AudioSource AudioSource;
+
+    bool introSound = true;
 
     // Use this for initialization
     void Start () {
@@ -17,5 +21,18 @@ public class BackgroundController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         Debug.Log(AudioSource.isPlaying);
+        if (introSound && !AudioSource.isPlaying) {
+            introSound = false;
+            AudioSource.clip = LoopSoundStart;
+            AudioSource.PlayOneShot(LoopSoundStart);
+        }
+
+        if (!introSound && !AudioSource.isPlaying) {
+            AudioSource.clip = LoopSoundLoop;
+            AudioSource.loop = true;
+            AudioSource.Play();
+        }
+
+
 	}
 }
